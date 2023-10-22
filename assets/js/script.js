9e69db09b105aa0d041c6c3bab3100a8712f0a98
chris <chrisj21283@gmail.com>
2023-10-22 19:28:50 -0400

    map layers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,48 @@
 ////////// Map and search section //////////
+  // Create the default layer
+  var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    noWrap: true,
+    maxZoom: 20
+  });
+    // Create alternative layers
+    var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      noWrap: true,
+      
+  });
+  var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    noWrap: true,
+  });
+  
+  var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    noWrap: true,
+  });
+  var NASAGIBS_ViirsEarthAtNight2012 = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
+    attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+    bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+    minZoom: 1,
+    maxZoom: 8,
+    format: 'jpg',
+    time: '',
+    noWrap: true,
+    tilematrixset: 'GoogleMapsCompatible_Level'
+  });
+
+  // Create a baseMaps 
+let baseMaps = {
+  "Dark Matter": CartoDB_DarkMatter,
+  "Street Map": streetmap,
+  
+  "World Map": Esri_WorldImagery,
+  
+
+  
+  };
 
 // Location IQ API key
 var key = "pk.ab52d604f1e0511146ebe97634a5b6d7";
@@ -6,7 +50,7 @@ var searchRadius = 10000;
 var searchCriteria = $("#interestsInput");
 var reset = $("#reset");
 var criteria = 'all';
-
+var favorites = $('#favorites');
 // Default coordinates for the map's center
 var defaultCenter = [39.8283, -98.5795];
 // Initialize latlng with the default center coordinates
@@ -43,6 +87,8 @@ var map = L.map('map', {
   zoomControl: false,
 });
 
+L.control.layers(baseMaps, null, { collapsed: false, color: "gray" }).addTo(map);
+
 // Get the user's location using the Geolocation API
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(
@@ -52,7 +98,7 @@ if ("geolocation" in navigator) {
       var userLng = position.coords.longitude;
       // Update latlng with the user's location
       latlng = { lat: userLat, lng: userLng };
-      map.setView([latlng.lat, latlng.lng], 14);
+      map.setView([latlng.lat, latlng.lng], 16);
 
       // Call the getWeather function with the user's location
       getWeather(latlng);
@@ -65,12 +111,12 @@ if ("geolocation" in navigator) {
       // Log the error message
       console.log(error.message);
       // Set the map center to the default center when geolocation fails or user denies access
-      map.setView(defaultCenter, 5);
+      map.setView(defaultCenter, 17);
     }
   );
 } else {
   // Set the map center to the default center when geolocation is not available
-  map.setView(defaultCenter, 5);
+  map.setView(defaultCenter, 17);
 }
 
 // Add the autocomplete text box and search using the Location IQ built-in geocoder
@@ -80,7 +126,7 @@ var geocoderControl = L.control.geocoder(key, {
   panToPoint: true,
   focus: true,
   position: "topleft",
-  zoom: 14,
+  zoom: 15,
 }).addTo(map);
 
 // Event listener for address selection change to run the functions
@@ -102,6 +148,19 @@ var selectedData = JSON.parse(localStorage.getItem('selectedData')) || [];
 selectedData.push(dataToStore);
 // Store the data as a single JSON string in local storage
 localStorage.setItem('selectedData', JSON.stringify(selectedData));
+var favButton = $('<button>', {
+  text: placeName,
+});
+
+favButton.click(function () {
+  // Handle the click event (e.g., center the map on the location)
+  map.setView([dataToStore.latlng.lat, dataToStore.latlng.lng], 13);
+  getWeather(dataToStore.latlng);
+  nearbyStuff(dataToStore.latlng, criteria);
+});
+
+favorites.append(favButton);
+
 
 // Log the data (optional)
 console.log('Data stored:', dataToStore);
@@ -190,5 +249,6 @@ function nearbyStuff(latlng, criteria) {
 
 reset.on('click', function(){
   selectedData = [];
+  favorites.empty();
   localStorage.setItem('selectedData', JSON.stringify(selectedData));
 })
