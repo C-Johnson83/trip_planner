80370d44939e7020b573a00e0808b98e18a3f61c
Chris Johnson <Chrisj21283@gmail.com>
2023-10-23 09:32:30 -0400

    added more icons for points of interest

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,29 @@
 ////////// Map and search section //////////
-  // Create the default layer
-  var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    noWrap: true,
-    maxZoom: 20
-  });
-    // Create alternative layers
-    var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      noWrap: true,
-      
-  });
-  var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-    noWrap: true,
-  });
-  
-  var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-    noWrap: true,
-  });
-  var NASAGIBS_ViirsEarthAtNight2012 = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
-    attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
-    bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
-    minZoom: 1,
-    maxZoom: 8,
-    format: 'jpg',
-    time: '',
-    noWrap: true,
-    tilematrixset: 'GoogleMapsCompatible_Level'
-  });
+// Create the default layer
+var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: 'abcd',
+  noWrap: true,
+  maxZoom: 20
+});
+// Create alternative layers
+var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  noWrap: true,
 
-  // Create a baseMaps 
+});
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+  noWrap: true,
+});
+
+
+// Create a baseMaps 
 let baseMaps = {
   "Dark Matter": CartoDB_DarkMatter,
   "Street Map": streetmap,
-  
   "World Map": Esri_WorldImagery,
-  
-
-  
-  };
+};
 
 // Location IQ API key
 var key = "pk.ab52d604f1e0511146ebe97634a5b6d7";
@@ -69,22 +50,17 @@ searchCriteria.on("change", function () {
     }
     var markLocation = L.marker([parseFloat(latlng.lat), parseFloat(latlng.lng)]).addTo(map);
     markLocation.bindPopup('hi');
-  // Call the nearbyStuff function with the updated criteria
-  nearbyStuff(latlng, criteria);
-});
+    // Call the nearbyStuff function with the updated criteria
+    nearbyStuff(latlng, criteria);
+  });
 })
 
-// Add layers to the map using built-in Unwired
-var streets = L.tileLayer.Unwired({
-  key: key,
-  scheme: "streets"
-});
 
 // Initialize the map
 var map = L.map('map', {
   zoom: 14,
   scrollWheelZoom: true,
-  layers: [streets],
+  layers: [Esri_WorldImagery],
   zoomControl: false,
 });
 
@@ -136,36 +112,36 @@ geocoderControl.on('select', function (event) {
   // Get the latitude and longitude of the selected location
   var latlng = event.latlng;
   var placeName = event.feature.name
-criteria = criteria
+  criteria = criteria
 
-var dataToStore = {
-  name: placeName,
-  latlng: latlng,
-  criteria: criteria
-};
+  var dataToStore = {
+    name: placeName,
+    latlng: latlng,
+    criteria: criteria
+  };
 
-var selectedData = JSON.parse(localStorage.getItem('selectedData')) || [];
+  var selectedData = JSON.parse(localStorage.getItem('selectedData')) || [];
 
-selectedData.push(dataToStore);
-// Store the data as a single JSON string in local storage
-localStorage.setItem('selectedData', JSON.stringify(selectedData));
-var favButton = $('<button>', {
-  text: placeName,
-});
+  selectedData.push(dataToStore);
+  // Store the data as a single JSON string in local storage
+  localStorage.setItem('selectedData', JSON.stringify(selectedData));
+  var favButton = $('<button>', {
+    text: placeName,
+  });
 
-favButton.click(function () {
-  icon.empty();
-  // Handle the click event (e.g., center the map on the location)
-  map.setView([dataToStore.latlng.lat, dataToStore.latlng.lng], 13);
-  getWeather(dataToStore.latlng);
-  nearbyStuff(dataToStore.latlng, criteria);
-});
+  favButton.click(function () {
+    icon.empty();
+    // Handle the click event (e.g., center the map on the location)
+    map.setView([dataToStore.latlng.lat, dataToStore.latlng.lng], 13);
+    getWeather(dataToStore.latlng);
+    nearbyStuff(dataToStore.latlng, criteria);
+  });
 
-favorites.append(favButton);
+  favorites.append(favButton);
 
 
-// Log the data (optional)
-console.log('Data stored:', dataToStore);
+  // Log the data (optional)
+  console.log('Data stored:', dataToStore);
 
   icon.empty();
   getWeather(latlng);
@@ -228,65 +204,179 @@ function nearbyStuff(latlng, criteria) {
   });
   var citymarker = L.marker([parseFloat(latlng.lat), parseFloat(latlng.lng)]).addTo(map);
   citymarker.bindPopup('hi');
-  
+
   // Fetch nearby locations
   fetch(nearbyQueryUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('nearby places data',data); // Check the retrieved data
-    data.forEach(function (place) {
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log('nearby places data', data); // Check the retrieved data
+      data.forEach(function (place) {
         console.log(place.type)
 
-// Create a custom marker icon for places of interest
+        // Create a custom marker icon for places of interest
 
-var churchIcon = L.icon({
-  iconUrl: './assets/images/icons8-church-50.png', // Replace with the path to your custom marker image
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
-});
-var cinemaIcon = L.icon({
-  iconUrl: './assets/images/icons8-documentary-64.png', // Replace with the path to your custom marker image
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
-});
-var hospitalIcon = L.icon({
-  iconUrl: './assets/images/icons8-hospital-64.png', // Replace with the path to your custom marker image
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
-});
-var hotelIcon = L.icon({
-  iconUrl: './assets/images/icons8-hotel-64.png', // Replace with the path to your custom marker image
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
-});
-var bankIcon = L.icon({
-  iconUrl: './assets/images/icons8-bank-48.png', // Replace with the path to your custom marker image
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
-});
-var customIcon = L.icon({
-  iconUrl: './assets/images/icons8-drop-of-blood-48.png', // Replace with the path to your custom marker image
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
-});
+        var airportIcon = L.icon({
+          iconUrl: './assets/images/icons8-airplane-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var atmIcon = L.icon({
+          iconUrl: './assets/images/icons8-atm-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var bankIcon = L.icon({
+          iconUrl: './assets/images/icons8-bank-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var busStationIcon = L.icon({
+          iconUrl: './assets/images/icons8-bus-station-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var cardealerIcon = L.icon({
+          iconUrl: './assets/images/icons8-car-rental-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var churchIcon = L.icon({
+          iconUrl: './assets/images/icons8-church-50.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var cinemaIcon = L.icon({
+          iconUrl: './assets/images/icons8-documentary-64.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var customIcon = L.icon({
+          iconUrl: './assets/images/icons8-drop-of-blood-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var restuarantIcon = L.icon({
+          iconUrl: './assets/images/icons8-fast-food-53.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var fuelIcon = L.icon({
+          iconUrl: './assets/images/icons8-fuel-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var gymIcon = L.icon({
+          iconUrl: './assets/images/icons8-gym-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var hospitalIcon = L.icon({
+          iconUrl: './assets/images/icons8-hospital-64.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var hotelIcon = L.icon({
+          iconUrl: './assets/images/icons8-hotel-64.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var parkIcon = L.icon({
+          iconUrl: './assets/images/icons8-park-40.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var parkingIcon = L.icon({
+          iconUrl: './assets/images/icons8-parking-64.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var pharmacyIcon = L.icon({
+          iconUrl: './assets/images/icons8-pharmacy-64.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var pubIcon = L.icon({
+          iconUrl: './assets/images/icons8-pub-64.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var railwayStationIcon = L.icon({
+          iconUrl: './assets/images/icons8-railway-station-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var schoolIcon = L.icon({
+          iconUrl: './assets/images/icons8-school-94.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var stadiumIcon = L.icon({
+          iconUrl: './assets/images/icons8-stadium-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var supermarketIcon = L.icon({
+          iconUrl: './assets/images/icons8-supermarket-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
+        var toiletIcon = L.icon({
+          iconUrl: './assets/images/icons8-toilet-48.png', // Replace with the path to your custom marker image
+          iconSize: [32, 32], // Set the size of the icon
+          iconAnchor: [16, 32], // Set the anchor point of the icon
+        });
 
 
-      if (place.type == 'place_of_worship') {
-        mapIcon = churchIcon;
-      } else if (place.type == 'cinema') {
-        mapIcon = cinemaIcon;
-      } else if(place.type == 'hospital') {
-        mapIcon = hospitalIcon;
-      } else if(place.type == 'hotel') {
-        mapIcon = hotelIcon;
-      } else if(place.type == 'bank') {
-        mapIcon = bankIcon;
-      } else {
-        mapIcon = customIcon
-      }
-        
+        if (place.type == 'place_of_worship') {
+          mapIcon = churchIcon;
+        } else if (place.type == 'airport') {
+          mapIcon = airportIcon;
+        } else if (place.type == 'atm') {
+          mapIcon = atmIcon;
+        } else if (place.type == 'bank') {
+          mapIcon = bankIcon;
+        } else if (place.type == 'bus_station') {
+          mapIcon = busStationIcon;
+        } else if (place.type == 'cardealer') {
+          mapIcon = cardealerIcon;
+        } else if (place.type == 'church') {
+          mapIcon = churchIcon;
+        } else if (place.type == 'cinema') {
+          mapIcon = cinemaIcon;
+        } else if (place.type == 'restuarant') {
+          mapIcon = restuarantIcon;
+        } else if (place.type == 'fuel') {
+          mapIcon = fuelIcon;
+        } else if (place.type == 'gym') {
+          mapIcon = gymIcon;
+        } else if (place.type == 'hospital') {
+          mapIcon = hospitalIcon;
+        } else if (place.type == 'hotel') {
+          mapIcon = hotelIcon;
+        } else if (place.type == 'park') {
+          mapIcon = parkIcon;
+        } else if (place.type == 'parking') {
+          mapIcon = parkingIcon;
+        } else if (place.type == 'pharmacy') {
+          mapIcon = pharmacyIcon;
+        } else if (place.type == 'pub') {
+          mapIcon = pubIcon;
+        } else if (place.type == 'railway_station') {
+          mapIcon = railwayStationIcon;
+        } else if (place.type == 'school') {
+          mapIcon = schoolIcon;
+        } else if (place.type == 'stadium') {
+          mapIcon = stadiumIcon;
+        } else if (place.type == 'supermarket') {
+          mapIcon = supermarketIcon;
+        } else if (place.type == 'toilet') {
+          mapIcon = toiletIcon;
+        } else {
+          mapIcon = customIcon
+        }
+
         // Create markers for each nearby place
         var marker = L.marker([parseFloat(place.lat), parseFloat(place.lon)], { icon: mapIcon }).addTo(map);
         marker.bindPopup(place.display_name);
@@ -294,7 +384,7 @@ var customIcon = L.icon({
     });
 }
 
-reset.on('click', function(){
+reset.on('click', function () {
   selectedData = [];
   favorites.empty();
   localStorage.setItem('selectedData', JSON.stringify(selectedData));
