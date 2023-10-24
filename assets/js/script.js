0d22b5799ebfd63215b98fdee125372a1e558942
IvanDirigible <sickeruncible@gmail.com>
2023-10-23 20:05:39 -0400

    Removed redundant code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,6 @@ var reset = $("#reset");
 var criteria = 'all';
 var favorites = $('#favorites');
 var interests = $('#interests');
-var poiAddBtn = $('#interestAdd');
 
 // Default coordinates for the map's center
 var defaultCenter = [39.8283, -98.5795]; // center of the united states
@@ -126,56 +125,6 @@ var geocoderControl = L.control.geocoder(key, {
   zoom: 15,
 }).addTo(map);
 
-// function readFavoritesFromStorage() {
-//     var favorite = localStorage.getItem('favorite');
-//     if (favorite) {
-//         favorite = JSON.parse(favorite);
-//     } else {
-//         favorite = [];
-//     }
-//     return favorite;
-// }
-
-// function saveFavoriteToStorage() {
-//     localStorage.setItem('favorite', JSON.stringify(favorite));
-// }
-
-// function printFavoriteData() {
-//     favoriteDisplayEl.empty();
-//     var favorite = readFavoritesFromStorage();
-//     for (var i = 0; i < favorite.length; i += 1) {
-//         var favorite = favorite[i];
-//         var rowEl = $('<tr>');
-//         var nameEl = $('<tr>').text(favorite.name);
-//         var poiEl = $('<td>').text(favorite.$(searchCriteria));
-//     }
-// }
-
-poiAddBtn.click(function(event) {
-    console.log(event);
-    var val = " ";
-    var a = document.getElementById("interestsInput");
-    for (var i = 0; i < a.length; i++) {
-        var option = a.options[i];
-        console.log("Here: " + a.options);
-    if (option.value == val) {
-        $(interests).append(option.text);
-        console.log("There: " + a.option);
-
-    }}
-    // selectPOI = document.querySelector('#interestsInput').textContent;
-    // output = selectPOI.textContent;
-    // var poiEl = $('<p></p>').text(document.querySelector('#interestsInput').textContent.selected);
-    // $(interests).append(poiEl);
-        // text: select.option.value,
-});
-
-
-
-
-
-
-
 //   listening event for address
   geocoderControl.on('select', function(event) {
     console.log (event);
@@ -195,7 +144,6 @@ poiAddBtn.click(function(event) {
   });
 // Event listener for address selection change to run the functions
 geocoderControl.on('select', function (event) {
-  console.log("Pizza" + event);
   // Get the latitude and longitude of the selected location
   var latlng = event.latlng;
   var placeName = event.feature.name
@@ -217,13 +165,11 @@ geocoderControl.on('select', function (event) {
   });
 
   favButton.click(function () {
-    console.log("this is the thing");
     icon.empty();
     // Handle the click event (e.g., center the map on the location)
     map.setView([dataToStore.latlng.lat, dataToStore.latlng.lng], 13);
     getWeather(dataToStore.latlng);
     nearbyStuff(dataToStore.latlng, criteria);
-    // repoReapersAway(latlng)
   });
 
   favorites.append(favButton);
@@ -475,46 +421,6 @@ function getWeather(latlng) {
       humidity.text(`Humidity: ${humidityVal}%`);
     });
 }
-
-
-
-// function repoReapersAway(latlng) {
-//   var drivingUrl = 'https://us1.locationiq.com/v1/directions/driving/';
-//   var polyline;
-//   var userLocationLngLat = {lng: -83.5085, lat: 31.4505};
-
-//   var drivingQueryUrl = drivingUrl + userLocationLngLat.lng+ ',' + userLocationLngLat.lat + ';' + latlng.lng + ',' + latlng.lat + "?key=" + key + '&steps=true&alternatives=true&geometries=geojson&overview=full';
-
-//   // Remove the old polyline if it exists
-//   if (polyline) {
-//     map.removeLayer(polyline);
-//   }
-
-//   startingPointMarker = L.marker(userLocationLngLat).addTo(map).bindPopup("Start the trip here");
-//   endingPointMarker = L.marker([latlng.lng, latlng.lat]).addTo(map).bindPopup("End the trip here");
-
-// fetch(drivingQueryUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log("driving data", data);
-
-      // // Extract the coordinates from the GeoJSON response
-      // var coordinates = data.routes[0].geometry.coordinates;
-
-      // // Create an empty array to store polyline coordinates
-      // var polylineCoordinates = [];
-
-      // // Add coordinates to the polylineCoordinates array
-      // coordinates.forEach(function (coordinate) {
-      //   polylineCoordinates.push([coordinate[1], coordinate[0]]);
-        
-      //   // Create the polyline using the coordinates
-      //   polyline = L.polyline(polylineCoordinates, { color: 'red' }).addTo(map);
-      // });
-//     });
-// }
 
 reset.on('click', function () {
   selectedData = [];
